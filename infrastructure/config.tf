e80366bfde877eeb66141cc12a92c7df84e3d3fb
Raza Ali <raza.ali@gfk.com>
2023-10-25 11:28:21 +0200

    running again

--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -1,21 +1,12 @@
 variable "aws_region" {
    default = "eu-central-1"
 }
-data "aws_availability_zones" "available_zones" {
-  state = "available"
-}
-variable "project" {
-   default = "mlops-cicd-tf-workshop"
-}
-variable "log_group_name" {
-   default = "ecs/mlops"
-}
+
+
 variable "user_name" {
   default     = "raza" # You can set a default value or provide it when running Terraform.
 }
-variable "validity" {
-   default = "2"
-}
+
 
 
 
